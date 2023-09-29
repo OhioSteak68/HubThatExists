b1915f62577cc5d23b693d49189fb5b48ee8983e
OhioSteak68 <144377840+OhioSteak68@users.noreply.github.com>
2023-09-29 19:27:33 +0200

    Update HubThatExists.lua

--- HubThatExists.lua
+++ HubThatExists.lua
@@ -1,4 +1,4 @@
-if game.PlaceId == 11520107397 or game.PlaceId == 9015014224 then --If you copy this ur a nerd
+if game.PlaceId == 6403373529 or game.PlaceId == 9015014224 then --If you copy this ur a nerd
     loadstring(game:HttpGet("https://raw.githubusercontent.com/OhioSteak68/HubThatExists/main/Slap%20Battles.lua"))() --If you copy this ur a nerd
 elseif game.PlaceId == 12611565148 then --If you copy this ur a nerd 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/OhioSteak68/HubThatExists/main/Alpha%20Killstreak.lua"))() --If you copy this ur a nerd
