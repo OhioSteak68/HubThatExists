7729a6c7c39497bcd66e4c647e82021d3788c158
OhioSteak68 <144377840+OhioSteak68@users.noreply.github.com>
2023-09-29 16:59:15 +0200

    Update HubThatExists.lua

--- HubThatExists.lua
+++ HubThatExists.lua
@@ -1,7 +1,7 @@
-if game.PlaceId == 11520107397 or game.PlaceId == 9015014224 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/OhioSteak68/HubThatExists/main/Slap%20Battles.lua"))()
-elseif game.PlaceId == 12611565148 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/OhioSteak68/HubThatExists/main/Alpha%20Killstreak.lua"))()
-else
-    error("GAME NOT SUPPORTED WITH HUB THAT EXISTS :D")
-end
+if game.PlaceId == 11520107397 or game.PlaceId == 9015014224 then --If you copy this ur a nerd
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/OhioSteak68/HubThatExists/main/Slap%20Battles.lua"))() --If you copy this ur a nerd
+elseif game.PlaceId == 12611565148 then --If you copy this ur a nerd 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/OhioSteak68/HubThatExists/main/Alpha%20Killstreak.lua"))() --If you copy this ur a nerd
+else --If you copy this ur a nerd
+    error("GAME NOT SUPPORTED WITH HUB THAT EXISTS :D") --If you copy this ur a nerd
+end --If you copy this ur a nerd
